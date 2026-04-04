9af6dba5872de587a1497f9e82b9b8eb24a78bc0
Aiden Davidson <aidavidson@chapman.edu>
2026-04-03 20:40:19 -0700

    Player Script version 1

---
+++ panther-hacks-2026/Scripts/Player.cs
@@ -0,0 +1,60 @@
+using Godot;
+using System;
+
+public partial class Player : CharacterBody2D
+{
+	public float playerSpeed;
+	public int handmadeTimer;
+	public int Oxygen;
+	public int Health;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		playerSpeed = 300f;
+		Oxygen = 100;
+		Health = 100;
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		if(Oxygen > 0 && Health > 0){
+			HandleMovement(delta);
+			handmadeTimer++;
+			handmadeTimer = OxygenManagement(handmadeTimer);
+		}else{
+			//figure out later
+			GD.Print("player is dead");
+		}
+		
+		
+	}
+	private void HandleMovement(double delta){
+		float Xmovement = 0f;
+		float Ymovement = 0f;
+		if(Input.IsKeyPressed(Key.W)){
+			Ymovement = -1f;
+		}else if(Input.IsKeyPressed(Key.S)){
+			Ymovement = +1f;
+		}else if(Input.IsKeyPressed(Key.A)){
+			Xmovement = -1f;
+		}else if(Input.IsKeyPressed(Key.D)){
+			Xmovement = +1f;
+		}
+		Vector2 MoveDirection = new Vector2(Xmovement, Ymovement).Normalized();
+		Position += MoveDirection * playerSpeed * (float)delta;
+	}
+	private int OxygenManagement(int timer){
+		
+		if(Position.Y <= 0){
+			Oxygen = 100;
+		}
+		if((timer % 100) == 0){
+			Oxygen--;
+			GD.Print(Oxygen);
+			return 0;
+		}
+		return timer;
+	}
+}
