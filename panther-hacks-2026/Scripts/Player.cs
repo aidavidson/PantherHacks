dce9baf1ee36044f7fa0f91890539c9f50de7b96
Ethan Yoo <eyoo@chapman.edu>
2026-04-05 12:15:57 -0700

    new ethan branch trying to push fish stuff

--- panther-hacks-2026/Scripts/Player.cs
+++ panther-hacks-2026/Scripts/Player.cs
@@ -10,10 +10,16 @@ public partial class Player : CharacterBody2D
 	private Sprite2D _sprite;
 	private Texture2D _leftTexture;
 	private Texture2D _rightTexture;
-		
-		
+	
+	[Export] public PackedScene projectileScene; // drag your harpoon.tscn here
+	public float harpoonSpeed = 500f;    // optional: speed of the harpoon
+	private float reloadTimer = 0f;               // internal timer for cooldown
+	[Export] public float reloadTime = 0.2f;      // time between shots
+	
 	public int OxygenTimer;
 	public int DamageTimer;
+	private float _timer = 0f;
+	private float _waitTime = 5.0f; // 5 seconds
 	
 	
 	//player survival vars
@@ -25,6 +31,7 @@ public partial class Player : CharacterBody2D
 	public float playerSpeed;
 	public int objsInBag;
 	public int fishSaved;
+	public int totalTrash;
 	
 	//manipulator of player class for the variables controlled by collisions
 	public static Player Instance;
@@ -32,21 +39,23 @@ public partial class Player : CharacterBody2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		//player is classified as a player
 		this.AddToGroup("player");
 		
+		//sprite node
 		_sprite = GetNode<Sprite2D>("Sprite2D");
+		//player textures
 		_rightTexture = GD.Load<Texture2D>("res://Sprites/Diver-1-Right.png");
 		_leftTexture = GD.Load<Texture2D>("res://Sprites/Diver1_big.png");
+		//harpoon scene
+		projectileScene = GD.Load<PackedScene>("res://Object_Scenes/Harpoon.tscn");
 		
-		
-	
-		
-		
-		
+		//default player aspects
 		playerSpeed = 300f;
 		Oxygen = 100;
 		Health = 100;
 		objsInBag = 0;
+		totalTrash = 0;
 		fishSaved = 0;
 		Instance = this;
 	}
@@ -54,14 +63,20 @@ public partial class Player : CharacterBody2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		GD.Print($"DEATH CAUSE - Health: {Health}, Oxygen: {Oxygen}");
-	
 		if(Oxygen > 0 && Health > 0){
+			LevelCompleted();
 			HandleMovement(delta);
 			OxygenTimer++;
 			OxygenTimer = OxygenManagement(OxygenTimer);
 			DamageTimer++;
 			DamageTimer = EnemyDamageHandling(DamageTimer);
+			_timer += (float)delta;
+			if (Input.IsMouseButtonPressed(MouseButton.Left) && _timer >= _waitTime){
+				GD.Print("Harpoon");
+				ShootHarpoon(GetGlobalMousePosition());
+				_timer = 0f;
+			}
+			
 		}else{
 			//figure out later
 			GD.Print("I am not commented");
@@ -73,7 +88,28 @@ public partial class Player : CharacterBody2D
 	}
 	
 	
-	
+	private void LevelCompleted(){
+		
+		Node globalMenu = GetNodeOrNull("/root/StartMenu");
+		if(totalTrash == 1){
+			if(globalMenu == null){
+				GD.Print("null");
+			}
+			GD.Print("hello");
+			bool isLevel2Unlocked = (bool)globalMenu.Get("level2Unlocked");
+			if(isLevel2Unlocked == false){
+				globalMenu.Set("level2Unlocked", true);
+				GD.Print("Is Level 2 unlocked");
+				GD.Print(globalMenu.Get("level2Unlocked"));
+			}else{
+				globalMenu.Set("level3Unlocked", true);
+				GD.Print("Is Level 3 unlocked");
+				GD.Print(globalMenu.Get("level3Unlocked"));
+			}
+			GetTree().ChangeSceneToFile("res://Menu/menu.tscn");
+			//test comment for github
+		}
+	}
 	
 	
 	
@@ -97,6 +133,13 @@ public partial class Player : CharacterBody2D
 		Velocity = velocity;
 		MoveAndSlide();
 	}
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	// manages the oxygen of the player
@@ -128,6 +171,28 @@ public partial class Player : CharacterBody2D
 		}
 		return timer;
 	}
+
+
+	private void ShootHarpoon(Vector2 MousePosition){
+		GD.Print("We In Da function");
+		if (projectileScene == null)
+		{
+			GD.PrintErr("CRITICAL: projectileScene is NULL! Check Inspector or File Path.");
+		}
+		Vector2 Direction = (MousePosition-GlobalPosition).Normalized();
+		var projectile = (RigidBody2D)projectileScene.Instantiate();
+		projectile.Position = GlobalPosition;
+		projectile.Rotation = Direction.Angle();
+		float speed = 500f;
+		projectile.LinearVelocity = Direction * speed;
+		GetParent().AddChild(projectile);
+		//hello
+	}
+	
+	
+	
+	
+	
 	
 
 }
